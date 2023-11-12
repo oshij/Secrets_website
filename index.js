566454b338e375d0dc938c645101ad333b79f71c
oshij <oshijtrivedi@gmail.com>
2023-11-12 17:13:21 +0530

    new project

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express"
+import axios from "axios"
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+const API_URL = "https://secrets-api.appbrewery.com/random";
+app.get("/", async (req,res) =>{
+    try{
+        const result = await axios.get(API_URL);
+        res.render("index.ejs",{secret : result.data.secret , user : result.data.username })
+    }
+    catch(error){
+        console.log(error.response.message);
+        res.status(500);
+    }
+})
+app.listen(port, () => {
+    console.log(`Server is running on PORT : ${port}`)
+})
